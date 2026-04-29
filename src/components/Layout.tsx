953489b4f13f648a1279308cf9c6088afc38f58d
Your Name <martinezkaponda@gmail.com>
2026-04-29 17:09:26 +0200

    Permanently hide all scrollbars - global CSS fix

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,9 +4,9 @@ import { Footer } from "./Footer";
 import { WhatsAppFab } from "./WhatsAppFab";
 
 export const Layout = () => (
-  <div className="min-h-screen flex flex-col overflow-x-hidden">
+  <div className="min-h-screen flex flex-col">
     <Navbar />
-    <main className="flex-1 w-full">
+    <main className="flex-1">
       <Outlet />
     </main>
     <Footer />
